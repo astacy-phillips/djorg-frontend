3223df565c1a57687c7ae19847b1ad44b129df09
Amanda Phillips <amanda@phillipsdev.org>
2018-05-31 17:43:13 -0500

    add new note with styled component and nicer gql style, and playing with csrf token

--- djorg/src/components/App.js
+++ djorg/src/components/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../styles/App.css';
-import NoteList from './NoteList';
+// import NoteList from './NoteList';
+import Note2 from './Note2';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <NoteList />
+        <Note2 />
       </div>
     );
   }
---
+++ djorg/src/components/Note2.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  border: 1px solid lightslategray;
+  border-radius: .4rem;
+  background-color: whitesmoke;
+  & div {
+    display: flex;
+    padding: 10px;
+    justify-content: space-around;
+  }
+  & h2 {
+    text-align: center;
+    font-family: Verdana, sans-serif;
+    font-size: 2rem;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  }
+`
+
+const Note = styled.div`
+  border: 1px;
+  display: flex;
+  flex-direction: column;
+  flex-basis: 250px;
+  margin: 0 5px 20px 5px;
+  border-radius: .1rem;
+  box-shadow: 0 10px 6px -6px rgba(119, 119, 119, .6);
+  background-color: lightcyan;
+
+  & h4 {
+  margin-top: 5px;
+  text-align: center;
+  border-bottom: 3px solid black;
+  border-top: 3px solid black;
+  font-family: Verdana, sans-serif;
+  font-size: 1.2rem;
+  padding-bottom: 5px;
+  padding-top: 5px;
+  margin-bottom: 5px;
+} 
+& p {
+  padding-left: 10px;
+  padding-right: 10px;
+  font-family: Arial, sans-serif;
+  line-height: 1.3rem;
+}
+`
+
+const Note2 = ({data: {loading, error, notes}}) => {
+    if (error) return <p>error</p>
+    if (!loading) {
+      return (
+        <Wrapper>
+          <h2>Djorgenstein Notes</h2>
+          <div>
+          {notes.map((note, index) => (
+            <Note key={index}>
+              <h4>{note.title}</h4>
+              <p>{note.content}</p>
+            </Note>
+          ))}
+          </div>
+        </Wrapper>
+      )
+    }
+    return <div>Loading...</div>
+  }
+  
+  export const query = gql`
+    query notes {
+      notes {
+      title
+      content
+      }
+    }
+    `
+  
+  export default graphql(query)(Note2)
